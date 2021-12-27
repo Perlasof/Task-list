8ecfa4502d5602e7e87c2f78ca3dfe790ea1575b
Perla <perlagonz@gmail.com>
2021-12-27 19:34:13 -0300

    Lista de tareas

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,34 @@
+import {Task} from "./Task"
+export const TASKS : Task[]= [
+{
+    id : 1 , 
+    text : "Terminar primer modulo de Angular" , 
+    day : 'Agosto 5 a las 12:00 hs' ,
+    reminder : true ,
+
+},
+{
+    id : 2 , 
+    text : "Hacer las compras para la cena" , 
+    day : 'Agosto 5 a las 17:00 hs' ,
+    reminder : true ,
+
+},
+{
+    id : 3 , 
+    text : "Investigar sobre Boostrap" , 
+    day : 'Agosto 5 a las 19:00 hs' ,
+    reminder : false ,
+
+},
+{
+    id : 4 , 
+    text : "Leer mi serie favorita" , 
+    day : 'Agosto 5 a las 21:00 hs' ,
+    reminder : false ,
+
+},
+
+
+
+]
